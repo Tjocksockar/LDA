dcc134ed4dc180af8d7a1224cb633b163aab86f9
KarlaRehn <36156493+KarlaRehn@users.noreply.github.com>
2020-01-01 16:26:07 +0100

    Fixed initialisation of beta
    Now it's dirichlet distributed!

--- VI.py
+++ VI.py
@@ -16,9 +16,8 @@ def VI(alpha, beta, data, k=10):
         for m in range(M):
             gamma_0[m, i] = alpha[i] + len(data[m][0]) / k
 
-    conv = 0.0001
     # phi = 1/k*np.ones([M, N, k])
-    gamma_new = 2 * gamma_0
+    gamma_new = 2*gamma_0.copy()
     phi = []
     # phi_new är unikt för varje dokument, phi ska fyllas med phi_new
 
@@ -33,18 +32,20 @@ def VI(alpha, beta, data, k=10):
 
     # WORD[m][n][0] index för ord n i dokument m
 
-    while LA.norm(gamma_new - gamma_0) > 1e-3:
+    while LA.norm(gamma_new - gamma_0) > 1:
         print(LA.norm(gamma_new - gamma_0))
         gamma_0 = gamma_new.copy()
         #print("ett varv till!")
+        
 
         for m in range(M):
-            phi_new = np.zeros([len(data[m][0]), k])
+            phi_new = np.ones([len(data[m][0]), k])/k
 
             for n in range(len(data[m][0])):
                 for i in range(k):
-                    phi_new[n, i] = beta[i, WORD[m][n][0]] * np.exp(digamma(gamma_0[m, i]))
+                    phi_new[n, i] = beta[i, WORD[m][n][0]] * np.exp(digamma(gamma_0[m, i])-digamma(np.sum(gamma_0[m,:])))
                 phi_new[n, :] = phi_new[n, :] / np.sum(phi_new[n, :])
+        
 
             gamma_new[m, :] = alpha + np.sum(phi_new, axis=0)
             phi.append(phi_new)
@@ -60,7 +61,12 @@ if __name__ == '__main__':
     number_of_documents = len(data)
 
     alpha = np.ones(number_of_topics) / number_of_topics
-    beta = np.ones([number_of_topics, voc_size]) / voc_size
+    beta = np.ones([number_of_topics, voc_size])
 
+    for k in range(number_of_topics):
+        beta[k, :] = np.random.dirichlet(np.ones(voc_size)/voc_size)
+        for i in range (voc_size):
+            if beta[k,i] == 0:
+                beta[k, i] = 0.00000000001
+    
     testVI = VI(alpha, beta, data, number_of_topics)
-    # print(testVI)
