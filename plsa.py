e28c6ecd054349ccc6dc3b45a8eea2a9baa62682
KarlaRehn <36156493+KarlaRehn@users.noreply.github.com>
2020-01-08 16:19:34 +0100

    Reading a textfile as testdata
    Note the split of the doc. This needs to be changed if we'd like to put in plain text instead of a file again.

--- plsa.py
+++ plsa.py
@@ -36,15 +36,17 @@ class TG(object):
 
 def main():
     parser = argparse.ArgumentParser(description='Probabilistic Latent Semantic Indexing')
-    parser.add_argument('--file', '-f', type=str,  required=True)
+    parser.add_argument('--file', '-f', type=str,  required=True) #description = "CSV-file with the training data"
+    parser.add_argument('--test', '-t', type=str,  required=True) #"txt-file with the test data"
     
     arguments = parser.parse_args()
     
     csv_file = arguments.file
+    testdata = arguments.test
 
-    #tg = TG(data)
-    #testset = TG('')
- 
+    tg = TG(testdata)
+    testdocs = tg.file_gen()
+    
     piplin = Pipeline(*DEFAULT_PIPELINE)
     piplin
     Korpus = Corpus.from_csv(csv_file, piplin)
@@ -52,8 +54,6 @@ def main():
     n_topics = 5
     plsa = PLSA(Korpus, n_topics, True)   
 
-    
-    #pdb.set_trace()
     result = plsa.fit()
     plsa
 
@@ -62,34 +62,29 @@ def main():
     eta = 0.00000001
     num_words = 0
     docprobs = [] #the document logprobability
-    testdocs = [['test', 'polyketide', 'involves', 'faster', 'lime', 'word', 'scientific', 'patient'], ['patient', 'and', 'here','another','with','some','more','word','medical']] #some text? the testset
+    #testdocs = [['test', 'polyketide', 'involves', 'faster', 'lime', 'word', 'scientific', 'patient'], ['patient', 'and', 'here','another','with','some','more','word','medical']] #some text? the testset
 
     for doc in testdocs:
+        words = doc.split()
         PW = 0        
         t_d = result.predict(doc)[0]
-        missingwords = result.predict(doc)[2] #array med tre element:
+        missingwords = result.predict(doc)[2] #predict är en array med tre element:
         # array med sannolikheten att doc tillhör topic i på plats i
-        #antal osedda ord
-        #lista med osedda ord
+        # antal osedda ord
+        # lista med osedda ord
         w_t = result.word_given_topic
         t_w = result.topic_given_word
-        #pdb.set_trace()
-        #print("true or false?", 'special' in Korpus.index.keys())
-
-        #t_w = t_w[windex]
-        for word in doc:
-            print(word)
+        
+        for word in words:
+            #print(word)
             if word not in Korpus.index.keys():
                 p_w = 0
             
             else:
-                print("this word is in the training corpus", word)
+                #print("this word is in the training corpus", word)
                 p_w = 0
-                #pdb.set_trace()
                 windex = Korpus.index.get(word)
 
-            #pdb.set_trace()
-            
                 for topic in range(n_topics):
                 
                     p_w = p_w+(w_t[topic][windex][1]*t_d[topic]/(t_w[topic][windex]+eta))
