fe8313b762b96b565d5b679680a83711db3d0db2
KarlaRehn <36156493+KarlaRehn@users.noreply.github.com>
2020-01-08 15:41:02 +0100

    Maybe a working perplexity?
    Needs a change in the source code for PSLA, the addition of     
    
        @property
        def topic_given_word(self) -> ndarray:
            """The relative importance of latent topics in each word.
            
            Dimensions are :math:`n_{topics} \\times n_{unique words}`
            """
            return self.__topic_given_word 
    
    in the result-module

--- plsa.py
+++ plsa.py
@@ -1,7 +1,7 @@
 from plsa import *
 from plsa.pipeline import DEFAULT_PIPELINE
 from plsa.algorithms import PLSA
-import plsa.preprocessors as pre
+import plsa.preprocessors as pre 
 from plsa.algorithms.result import *
 import codecs
 import argparse
@@ -9,7 +9,9 @@ import os
 import pdb
 import numpy as np
 
+ 
 
+    
 class TG(object):
     def __init__(self, data):
         self.data = data
@@ -17,76 +19,91 @@ class TG(object):
     def dir_gen(self):
         dir_name = self.data
         fnames = [os.path.join(dir_name, fn) for fn in os.listdir(dir_name)]
-
+    
         for fname in fnames:
             korp = open(fname, encoding='utf8', errors='ignore')
             korp = korp.read().lower()
             yield korp
-
-    def file_gen(self):
+    
+    def file_gen(self):    
         fname = self.data
         f = open(fname, encoding='utf8', errors='ignore')
         f = f.read()
         abstracts = f.split("\n")
-        for abstract in abstracts:
+        for abstract in abstracts: 
             yield abstract
 
 
 def main():
-    #parser = argparse.ArgumentParser(description='Probabilistic Latent Semantic Indexing')
-    #parser.add_argument('--file', '-f', type=str, required=True)
+    parser = argparse.ArgumentParser(description='Probabilistic Latent Semantic Indexing')
+    parser.add_argument('--file', '-f', type=str,  required=True)
+    
+    arguments = parser.parse_args()
+    
+    csv_file = arguments.file
 
-    #arguments = parser.parse_args()
-
-    csv_file = "data/Splitted/Test_400.csv"
-
-    # tg = TG(data)
-    # testset = TG('')
-
+    #tg = TG(data)
+    #testset = TG('')
+ 
     piplin = Pipeline(*DEFAULT_PIPELINE)
     piplin
     Korpus = Corpus.from_csv(csv_file, piplin)
 
     n_topics = 5
-    plsa = PLSA(Korpus, n_topics, True)
+    plsa = PLSA(Korpus, n_topics, True)   
 
-    # pdb.set_trace()
+    
+    #pdb.set_trace()
     result = plsa.fit()
     plsa
 
     result.topic
-
+    
     eta = 0.00000001
     num_words = 0
-    docprobs = []  # the document logprobability
-    testdocs = [['test', 'text', 'special', 'words', 'scientific', 'patients'],
-                ['and' 'heres' 'another' 'with' 'some' 'more' 'words' 'medical']]  # some text? the testset
+    docprobs = [] #the document logprobability
+    testdocs = [['test', 'polyketide', 'involves', 'faster', 'lime', 'word', 'scientific', 'patient'], ['patient', 'and', 'here','another','with','some','more','word','medical']] #some text? the testset
 
     for doc in testdocs:
-        PW = 0
-        result=result.predict(doc)
-        t = result.predict(doc)[0]
+        PW = 0        
+        t_d = result.predict(doc)[0]
+        missingwords = result.predict(doc)[2] #array med tre element:
+        # array med sannolikheten att doc tillhör topic i på plats i
+        #antal osedda ord
+        #lista med osedda ord
+        w_t = result.word_given_topic
+        t_w = result.topic_given_word
+        #pdb.set_trace()
+        #print("true or false?", 'special' in Korpus.index.keys())
 
-        t_w = result.topic_given_doc
-        t_w = t_w[windex]
+        #t_w = t_w[windex]
         for word in doc:
-            if word in result[2]:
-                w_t=eta
+            print(word)
+            if word not in Korpus.index.keys():
+                p_w = 0
+            
             else:
-                windex = Korpus.index[word]
-                pdb.set_trace()
+                print("this word is in the training corpus", word)
+                p_w = 0
+                #pdb.set_trace()
+                windex = Korpus.index.get(word)
+
+            #pdb.set_trace()
+            
                 for topic in range(n_topics):
-                    w_t = result.word_given_topic  # topics*voc_size*2??
-                    w_t = w_t[n][windex][1]
-
-            PW = np.log(w_t * t / (t_w + eta) + eta) + PW
-
+                
+                    p_w = p_w+(w_t[topic][windex][1]*t_d[topic]/(t_w[topic][windex]+eta))
+                 
+            
+            PW = np.log(p_w+eta)+PW
+                
             num_words += 1
+        print("Logprob for a document!", PW)
         docprobs.append(PW)
 
-    perplexity = np.exp(np.sum(docprobs) / num_words)
+    perplexity = np.exp(-(np.sum(docprobs)/num_words))
     print("Perplexity", perplexity)
-
+ 
 
 if __name__ == "__main__":
     main()
