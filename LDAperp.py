a6fe142909a2403e10343b97f0684d263fb8776c
Labb1 <fangming.yan.yolanda@gmail.com>
2020-01-13 15:00:49 +0100

    Works together  with em now

--- LDAperp.py
+++ LDAperp.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.special import gamma, digamma
 from read_data import onehot_encoder
 import pdb
+from operator import itemgetter
 from em import em
 from VI import VI
 
@@ -14,15 +15,13 @@ def lower_bound(g, phi, alfa, beta, doc, voc_size, num_topics):
     
     
     E1 = np.log(gamma(np.sum(alfa)))-np.sum(np.log(gamma(alfa)))+np.sum(
-    (alfa-1)*(digamma(g)-digamma(np.sum(g)))    
-    )
+        (alfa-1)*(digamma(g)-digamma(np.sum(g))))
     E2 = np.sum(phi * (digamma(g) - digamma(np.sum(g)))) #här är dimenstionerna otydliga asssssså
-    #np.sum(np.sum(phi, axis = 0), axis = 1)
     E3 = 0
-    #pdb.set_trace()
     for n in range(len(doc)):
         for i in range(num_topics):
-                E3 += phi[n][i]*np.log(beta[i, doc[n]])
+                #pdb.set_trace()
+                E3 += phi[n,i]*np.log(beta[i, doc[n][0]])
 
     E4 = - np.log(gamma(np.sum(g))) + np.sum(np.log(gamma(g)))-np.sum((g-1)*(digamma(g)-digamma(np.sum(g))))
     
@@ -30,8 +29,6 @@ def lower_bound(g, phi, alfa, beta, doc, voc_size, num_topics):
     for n in range(len(doc)):
         for i in range(num_topics):
             E5 += phi[n, i]*np.log(phi[n, i])
-    
-    #pdb.set_trace()
 
 
     LB = E1 + E2 + E3 + E4 - E5
@@ -42,30 +39,22 @@ def perplexity(g, phi, alfa, beta, data, voc_size, num_topics):
     # inte 'ettord' utan typ 1938, voc_index
     logp = 0
     num_words = 0
-    for index, doc in enumerate(data):
+    for index in range(len(data)):
         logp += logp
-        logp = lower_bound(g[index], phi[index], alfa, beta, doc, voc_size, num_topics)
-        num_words += len(doc)
-    perp = np.exp(-logp/num_words)
+        logp = lower_bound(g[index], phi[index], alfa, beta, data[index], voc_size, num_topics)
+        num_words += len(data[index])
+
+    perp = np.exp(-np.sum(logp)/num_words)
+    #perp=np.sum(perp)
     return perp
 
 def main():
-    one_hot = onehot_encoder('523preprocessed_abstracts_data.csv')
+    one_hot, word_to_ind, ind_to_word = onehot_encoder('200preprocessed_abstracts_data_test.csv')
     num_topics = 3
-    K = num_topics
-    voc_size = len(one_hot[0][0])
-    #voc_size = 10
-    M = len(one_hot[0])
-    #M = 2
+    voc_size = one_hot[0].shape[0]
+    M = len(one_hot)
 
-    #data = []
-
-
-        #WORD[m][n]
-
-    #data = [[0,1,1,1,3,4,3,3,2,1,1,1], [3,4,5,6,8,8,9,5,5,5,6,7]]
-    alpha, beta = em(num_topics, voc_size, one_hot)
-    phi, gamma = VI(alpha, beta, one_hot, num_topics)
+    alpha, beta, phi,gamma = em(num_topics, voc_size, one_hot)
     
     data  = []
     for m in range(M):
@@ -73,25 +62,9 @@ def main():
         word = sorted(indx, key=itemgetter(1))
         data.append(word)
 
-    #alpha = np.ones(num_topics) / num_topics
-    #beta = np.ones([num_topics, voc_size])
-    #phi = []
-
-    #for m in range(len(data)):
-    #    phi.append(np.ones([len(data[m]), K])/K)
-
-    #gamma = np.zeros([len(data[m]), K])
-    #for i in range(K):
-    #    for m in range(M):
-    #        gamma[m, i] = alpha[i] + len(data[m]) / K
-
-    #for k in range(num_topics):
-    #    beta[k, :] = np.random.dirichlet(np.ones(voc_size))
-    #    for i in range (voc_size):
-    #        if beta[k,i] == 0:
-    #            beta[k, i] += 0.00001
 
     perplx = perplexity(gamma, phi, alpha, beta, data, voc_size, num_topics)
+
     print("Perplexity", perplx)
 
 if __name__ == "__main__":
