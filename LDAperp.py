ccc80bdf480c6e2416544b8c716e09bc7a9b8b9d
Fangming Yan <33481206+Yolitoxy@users.noreply.github.com>
2020-01-14 10:59:57 +0100

    save fix

--- LDAperp.py
+++ LDAperp.py
@@ -57,34 +57,37 @@ def perplexity(g, phi, alfa, beta, data, voc_size, num_topics):
 def main():
     data, word_to_ind, ind_to_word = onehot_encoder('2200preprocessed_abstracts_data.csv')
     train=data[0:2000]
-    test=data[2000:2200]
+    #est=data[2000:2200]
     num_topics =15
     voc_size = data[0].shape[0]
-    M = len(test)
+    #M = len(test)
 
     alpha, beta, phi,gamma = em(num_topics, voc_size, train)
     
     testdata  = []
-    for m in range(M):
-        indx = np.argwhere(test[m] == np.amax(test[m]))
-        word = sorted(indx, key=itemgetter(1))
-        testdata.append(word)
+    #for m in range(M):
+       # indx = np.argwhere(test[m] == np.amax(test[m]))
+       # word = sorted(indx, key=itemgetter(1))
+       # testdata.append(word)
 
 
     #perplx = perplexity(gamma, phi, alpha, beta, testdata, voc_size, num_topics)
     csv_file_alpha = open(str(num_topics) + 'topics_alpha.csv', 'w')
     savetxt(csv_file_alpha,alpha, delimiter=',')
-    #csv_file.write(alpha)
-    csv_file.close()
-    csv_file = open(str(num_topics) + 'topics_beta.csv', 'w')
+    csv_file_alpha.close()
+
+    csv_file_beta = open(str(num_topics) + 'topics_beta.csv', 'w')
     savetxt(csv_file_beta, beta, delimiter=',')
-    csv_file.close()
-    csv_file = open(str(num_topics) + 'topics_gamma.csv', 'w')
+    csv_file_beta.close()
+
+    csv_file_gamma = open(str(num_topics) + 'topics_gamma.csv', 'w')
     savetxt(csv_file_gamma, gamma, delimiter=',')
-    csv_file.close()
-    csv_file = open(str(num_topics) + 'topics_phi.csv', 'w')
-    savetxt(csv_file_phi, phi, delimiter=',')
-    csv_file.close()
+    csv_file_gamma.close()
+
+    csv_file_phi = open(str(num_topics) + 'topics_phi.csv', 'w')
+    for m in range(len(train)):
+        savetxt(csv_file_phi, phi[m], delimiter=',')
+    csv_file_phi.close()
 
     #print("Perplexity", perplx)
 
