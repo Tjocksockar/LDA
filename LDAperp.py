5bfe4e5837989122c99fa5aeb421490c41015eac
Labb1 <fangming.yan.yolanda@gmail.com>
2020-01-14 09:16:47 +0100

    save values of parameters

--- LDAperp.py
+++ LDAperp.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy import asarray
+from numpy import savetxt
 from scipy.special import gamma, digamma
 from read_data import onehot_encoder
 import pdb
@@ -53,23 +55,38 @@ def perplexity(g, phi, alfa, beta, data, voc_size, num_topics):
     return perp
 
 def main():
-    one_hot, word_to_ind, ind_to_word = onehot_encoder('2000preprocessed_abstracts_data.csv')
-    num_topics =7
-    voc_size = one_hot[0].shape[0]
-    M = len(one_hot)
+    data, word_to_ind, ind_to_word = onehot_encoder('2200preprocessed_abstracts_data.csv')
+    train=data[0:2000]
+    test=data[2000:2200]
+    num_topics =15
+    voc_size = data[0].shape[0]
+    M = len(test)
 
-    alpha, beta, phi,gamma = em(num_topics, voc_size, one_hot)
+    alpha, beta, phi,gamma = em(num_topics, voc_size, train)
     
-    data  = []
+    testdata  = []
     for m in range(M):
-        indx = np.argwhere(one_hot[m] == np.amax(one_hot[m]))
+        indx = np.argwhere(test[m] == np.amax(test[m]))
         word = sorted(indx, key=itemgetter(1))
-        data.append(word)
+        testdata.append(word)
 
 
-    perplx = perplexity(gamma, phi, alpha, beta, data, voc_size, num_topics)
+    #perplx = perplexity(gamma, phi, alpha, beta, testdata, voc_size, num_topics)
+    csv_file_alpha = open(str(num_topics) + 'topics_alpha.csv', 'w')
+    savetxt(csv_file_alpha,alpha, delimiter=',')
+    #csv_file.write(alpha)
+    csv_file.close()
+    csv_file = open(str(num_topics) + 'topics_beta.csv', 'w')
+    savetxt(csv_file_beta, beta, delimiter=',')
+    csv_file.close()
+    csv_file = open(str(num_topics) + 'topics_gamma.csv', 'w')
+    savetxt(csv_file_gamma, gamma, delimiter=',')
+    csv_file.close()
+    csv_file = open(str(num_topics) + 'topics_phi.csv', 'w')
+    savetxt(csv_file_phi, phi, delimiter=',')
+    csv_file.close()
 
-    print("Perplexity", perplx)
+    #print("Perplexity", perplx)
 
 if __name__ == "__main__":
     main()
